a89dae347a34a3941ad97f33f4e8607de82136d9
Tim Branyen <tim@tabdeveloper.com>
2015-01-24 18:47:51 -0500

    Small card styles

--- app/components/small-card.js
+++ app/components/small-card.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'li',
+  tagName: 'div',
   classNames: ['small-card'],
   classNameBindings: ['selected'],
   data: null,
