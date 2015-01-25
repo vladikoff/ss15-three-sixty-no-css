a76d5babddf246be0037d9eb20113892774c1ca1
Vlad Filippov <vlad.filippov@gmail.com>
2015-01-24 22:22:26 -0500

    aaaa

--- vendor/soundtrack.js
+++ vendor/soundtrack.js
@@ -1,10 +1,10 @@
 var SOUNDTRACK = {
   init: function () {
-    var conductor = new BandJS();
-    conductor.setTimeSignature(4,4);
-    conductor.setTempo(120);
-    var piano = conductor.createInstrument();
-    piano.note('quarter', 'A4');
+    this.conductor = new BandJS();
+    this.conductor.setTimeSignature(4,4);
+    this.conductor.setTempo(120);
+    var piano = this.conductor.createInstrument();
+    piano.note('quarter', 'A4');/*
     piano.note('quarter', 'A4');
     piano.note('quarter', 'C4');
     piano.note('quarter', 'C4').rest('quarter');
@@ -18,9 +18,9 @@ var SOUNDTRACK = {
     piano.note('quarter', 'F4');
     piano.note('quarter', 'D4');
     piano.note('quarter', 'A4');
-    piano.note('quarter', 'A4');
+    piano.note('quarter', 'A4');*/
 
-    this.player = conductor.finish();
+    this.player = this.conductor.finish();
     this.player.play();
   },
   hit: function () {
