cd4c180882cfee216263fea371c5ed1d8b32efdf
Kyle Robinson Young <kyle@dontkry.com>
2015-01-25 01:09:22 -0800

    theres ya prob

--- app/views/game.js
+++ app/views/game.js
@@ -49,7 +49,7 @@ export default Ember.View.extend({
     }
 
     var cardId = this.get('controller.board' + key)
-    var position = key.replace(owner, '')
+    var position = key.slice(-2)
     this.get('controller').lookupCard(cardId).then((card) => {
       Ember.Logger.info('Creating mesh for card', position, owner, card.get('id'));
       util.addCard(position, owner, card)
