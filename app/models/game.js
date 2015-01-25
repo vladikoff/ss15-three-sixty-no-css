e0e5211ee214312f4f6f52a276eb9133f91795db
Vlad Filippov <vlad.filippov@gmail.com>
2015-01-24 20:37:24 -0500

    add game board to state

--- app/models/game.js
+++ app/models/game.js
@@ -5,4 +5,21 @@ export default DS.Model.extend({
   createdAt: DS.attr('number', { defaultValue: function() { return moment().utc() } }),
   turn: DS.attr('string', { defaultValue: '' }),
   lastTurnSwitch: DS.attr('number', { defaultValue: function() { return moment().utc() } }),
+  // Board Configuration
+  // Creator is the ID, aka the HOST
+  boardCreatorL1: DS.attr('string'),
+  boardCreatorL2: DS.attr('string'),
+  boardCreatorC1: DS.attr('string'),
+  boardCreatorC2: DS.attr('string'),
+  boardCreatorR1: DS.attr('string'),
+  boardCreatorR2: DS.attr('string'),
+  // Opponents board
+  boardOpponentL1: DS.attr('string'),
+  boardOpponentL2: DS.attr('string'),
+  boardOpponentC1: DS.attr('string'),
+  boardOpponentC2: DS.attr('string'),
+  boardOpponentR1: DS.attr('string'),
+  boardOpponentR2: DS.attr('string'),
+  creatorHealth: 10,
+  opponentHealth: 10
 });
