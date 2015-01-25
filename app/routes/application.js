d57668b83c563e3557fbc719eb2a8d49d7beb0ef
Kyle Robinson Young <kyle@dontkry.com>
2015-01-24 23:08:47 -0800

    Add tick to app cuz we dont care

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,5 +5,10 @@ export default Ember.Route.extend({
     logout: function () {
       location.reload();
     },
+    tick: function() {
+      // Just gobblin your tick action call
+      // because game.actions.tick calls it in a Ember.run.later
+      // this could be hit and we dont care
+    },
   },
 });
